18626416be4dfe41c4d4a76f622b0682e2c172e8
Mouctar <47394031+mamadou-mouctar-diallo@users.noreply.github.com>
2024-02-21 00:44:56 +0100

    implementation du Client Rest pour l'api pour l'appel de l'api back et de la vue des books sur l'appli windows

--- MAUI.Reader/Model/Author.cs
+++ MAUI.Reader/Model/Author.cs
@@ -1,13 +1,11 @@
-﻿
+
+using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
-using System.Text.Json.Serialization;
-using Newtonsoft.Json;
-using JsonConverter = System.Text.Json.Serialization.JsonConverter;
 
 namespace MAUI.Reader.Model
 {
-    public class Book
+    public class Author
     {
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         [Key]
@@ -16,14 +14,10 @@ namespace MAUI.Reader.Model
         // Mettez ici les propriété de votre livre: Nom, Autheur, Prix, Contenu et Genres associés
         // N'oublier pas qu'un livre peut avoir plusieur genres
         
-        public string Name { get; set; }
+        public string FirstName { get; set; }
 
-        public  ICollection<Author> Authors { get; set; }
+        public string LastName { get; set; }
 
-        public decimal Price { get; set; }
-
-        public string Content { get; set; }
-
-        public virtual ICollection<Genre> Genres { get; set; } = new List<Genre>();
+        public virtual ICollection<Genre> Book { get; set; } = new List<Genre>();
     }
 }
