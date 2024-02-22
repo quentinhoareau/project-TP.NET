ab518251fa25a8f10c6263ef2fac1d66d491428c
Mouctar <47394031+mamadou-mouctar-diallo@users.noreply.github.com>
2024-02-22 15:19:57 +0100

    Implementation du speech SpeechSynthesizer avec clique droite et lecture du text

--- MAUI.Reader/ViewModel/ReadBook.cs
+++ MAUI.Reader/ViewModel/ReadBook.cs
@@ -5,9 +5,24 @@ using MAUI.Reader.Model;
 
 namespace MAUI.Reader.ViewModel
 {
-    public partial class ReadBook : INotifyPropertyChanged
+    public partial class ReadBook: INotifyPropertyChanged
     {
         
+
+        public int CursorPosition
+        {
+            get;
+            set;
+        }
+
+
+        public int SelectedTextLength
+        {
+            get;
+            set;
+        }
+
+
         public Book ReceivedBook { get; set; }
         public SpeechSynthesizer synth { get; set; }
         public string Controller { get; set; }
@@ -15,16 +30,23 @@ namespace MAUI.Reader.ViewModel
         {
             Controller = "read";
             ReceivedBook = book;
-        }
-        public event PropertyChangedEventHandler PropertyChanged;
-        public void Read()
-        {
-            
             synth = new SpeechSynthesizer();
             synth.SetOutputToDefaultAudioDevice();
-            synth.SpeakCompleted += RestartReading;
-            synth.SpeakAsync(ReceivedBook.Content.ToString());
         }
+        [RelayCommand]
+        public void Read()
+        {
+            string textToSpeech = ReceivedBook.Content.ToString();
+            string selectedText = ReceivedBook.Content[this.CursorPosition..(this.CursorPosition + this.SelectedTextLength)];
+            Console.WriteLine(selectedText);
+            if (selectedText != "")
+            {
+                textToSpeech = selectedText;
+            }
+            synth.SpeakCompleted += RestartReading;
+            synth.SpeakAsync(textToSpeech);
+        }
+        [RelayCommand]
         public void Pause()
         {
             synth.Pause();
@@ -36,7 +58,7 @@ namespace MAUI.Reader.ViewModel
             synth.SpeakAsyncCancelAll();
             Controller = "read";
         }
-
+        [RelayCommand]
         public void Resume()
         {
             synth.Resume();
@@ -67,11 +89,6 @@ namespace MAUI.Reader.ViewModel
         private void RestartReading(Object sender, SpeakCompletedEventArgs eventArgs)
         {
             Controller = "read";
-        }
-
-        public void OnSelectText(Entry entry, EventArgs e)
-        {
-            
         }
     }
 
